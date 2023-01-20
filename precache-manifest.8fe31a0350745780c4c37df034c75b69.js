29ec656a6102fdbeaf350404317c34601b6fb447
luhgar <juniorjumper46@.com>
2023-01-20 18:05:28 +0530

    Updates

---
+++ precache-manifest.8fe31a0350745780c4c37df034c75b69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f95489e1750735fe4be4",
+    "url": "/api-fetching-reactjs/static/css/main.c3da2915.chunk.css"
+  },
+  {
+    "revision": "f95489e1750735fe4be4",
+    "url": "/api-fetching-reactjs/static/js/main.f95489e1.chunk.js"
+  },
+  {
+    "revision": "308a304c7f4658dcccae",
+    "url": "/api-fetching-reactjs/static/js/1.308a304c.chunk.js"
+  },
+  {
+    "revision": "de9481481275ee1ff21d",
+    "url": "/api-fetching-reactjs/static/js/runtime~main.de948148.js"
+  },
+  {
+    "revision": "00dcf57404758c56dabbf4a8c13978bf",
+    "url": "/api-fetching-reactjs/index.html"
+  }
+];
